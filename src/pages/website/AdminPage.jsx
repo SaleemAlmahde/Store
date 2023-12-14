63108a1ce2378dd5424ee5451c310dada07ea067
SaleemAlmahde <almahdesaleem@gmail.com>
2023-12-14 15:18:28 +0300

    .

---
+++ src/pages/website/AdminPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import AdminLayout from '../layouts/AdminLayout';
+
+const AdminPage = () => {
+    return (
+        <AdminLayout>
+            <div color='red' >hilckafilanfil</div>
+        </AdminLayout>
+    );
+}
+
+export default AdminPage;
